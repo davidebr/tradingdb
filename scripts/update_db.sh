884a38011dd8715b2427b3a854ed91069e362abf
Davide Branduardi <davide.branduardi@gmail.com>
2023-07-31 03:26:09 +0100

    screener

--- scripts/update_db.sh
+++ scripts/update_db.sh
@@ -1,1 +1,2 @@
-python update_db.py --db ../data/master.db --interval 1d #--time_str "2023-02-01 00:00:00"
+#python update_db.py --db ../data/master.db --interval 1d #--time_str "2023-02-01 00:00:00"
+python update_db.py --db ../data/master.db --interval 1h #--time_str "2023-02-01 00:00:00"
